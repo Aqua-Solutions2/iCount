64a055a80fda595ea1bf48236a04fc789ad6eaa5
Siebe Baree <siebe.baree@outlook.com>
2021-05-12 14:58:26 +0200

    checking on ID instead of name

--- cogs/help.py
+++ cogs/help.py
@@ -27,7 +27,7 @@ class HelpMsg(commands.Cog):
 
         db.close()
 
-        if ctx.author == ctx.guild.owner:
+        if ctx.author.id == ctx.guild.owner.id:
             embed = discord.Embed(
                 description=f"**__Owner Commands__**\n"
                             f"\n• **{prefix}resetall** - Remove all data from the bot in the guild."

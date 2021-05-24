274e73409b73cc4651c2675930fdf3b465b5803d
Siebe Baree <siebe.baree@outlook.com>
2021-05-24 18:46:43 +0200

    fixed import.py

--- cogs/import.py
+++ cogs/import.py
@@ -1,10 +1,8 @@
-import discord
 from discord.ext import commands
 import settings
 import mysql.connector
 import json
-import os
-from virustotal_python import Virustotal
+from core import _checks as checks
 
 
 class CogsImport(commands.Cog):
@@ -13,152 +11,135 @@ class CogsImport(commands.Cog):
         self.client = client
 
     @commands.command(name="import")
-    # @commands.is_owner()
     async def import_cmd(self, ctx):
-        if False and str(ctx.message.attachments) == "[]":
-            await ctx.send("You have to upload a file (.json) to this command.")
-            return
-        else:
-            # split_filename = str(ctx.message.attachments).split("filename='")[1]
-            # bestandsnaam = str(split_filename).split("' ")[0]
-            bestandsnaam = ""
-
-            if True or bestandsnaam.endswith(".json"):
-                # try:
-                #     os.remove(f"data/importdata/{ctx.guild.id}.json")
-                # except Exception:
-                #     pass
-
-                # import_bestand = await ctx.message.attachments[0].save(fp=f"data/importdata/{ctx.guild.id}.json")
-
-                file_path = f"data/importdata/eicar.json"
-                bestand = {
-                    "file": (os.path.basename(file_path), open(os.path.abspath(file_path), "rb"))
-                }
-            else:
-                await ctx.send("The file needs to have the extension `.json`. Other filetypes are not supported.")
+        if ctx.author.id == ctx.guild.owner_id:
+            if str(ctx.message.attachments) == "[]":
+                await ctx.send(":x: You have to upload a file (.json) to this command.")
                 return
 
-        with Virustotal(API_KEY=settings.vtotal_api_key, API_VERSION="v3") as vtotal:
-            return_data = vtotal.request("files", files=bestand, method="POST")
+            if ctx.message.attachments[0].size > 25000:
+                await ctx.send(":x: The file is larger than 25kb, we do not allow files bigger than 25kb.")
+                return
 
-        return
+            split_filename = str(ctx.message.attachments).split("filename='")[1]
+            bestandsnaam = str(split_filename).split("' ")[0]
 
-        with open(f"data/importdata/{ctx.guild.id}.json", "r") as import_file:
-            data = json.load(import_file)
+            if bestandsnaam.lower().endswith(".json"):
+                import_bestand = await ctx.message.attachments[0].read()
+            else:
+                await ctx.send(":x: The file needs to have the extension `.json`. Other filetypes are not supported.")
+                return
 
-        prefix = data['prefix']
-        channel_id = data['channel-id']
-        maxcount = data['maxcount']
-        timeoutrole = data['timeoutrole']
-        language = data['language']
-        count = data['count']
-        lastuser = data['lastuser']
-        modules = data['modules']
-        userdata = data['userdata']
-        notifications = data['notifications']
-        automation = data['automation']
+            if not str(import_bestand).startswith("b'{"):
+                await ctx.send(":x: Invalid JSON-file.")
+                return
 
-        import_file.close()
+            data = json.loads(import_bestand)
 
-        db = mysql.connector.connect(host=settings.host, database=settings.database,
-                                     user=settings.user, passwd=settings.passwd)
-        cursor = db.cursor()
+            try:
+                guild_id = data["guild-id"]
+                prefix = data['prefix']
+                channel_id = data['channel-id']
+                maxcount = data['maxcount']
+                timeoutrole = data['timeoutrole']
+                language = 'en'  # data['language']  # Coming Soon
+                count = data['count']
+                lastuser = data['lastuser']
+                modules = data['modules']
+                userdata = data['userdata']
+                notifications = data['notifications']
+                automation = data['automation']
+            except KeyError:
+                await ctx.send(":x: Invalid JSON-file.")
+                return
 
-        cursor.execute("SELECT * FROM guildSettings WHERE guild = %s", (ctx.guild.id,))
-        guild_settings = cursor.fetchone()
+            check = checks.Checks(ctx)
+            prefix = check.prefix(prefix)
+            maxcount = check.maxcount(maxcount)
+            lastuser = check.ids(lastuser)
+            count = check.count(count, maxcount)
+            channel_id = check.id_in_guild(channel_id, guild_id)
+            timeoutrole = check.id_in_guild(timeoutrole, guild_id)
+            modules = check.modules(modules)
 
-        if guild_settings is None:
-            print(f"[ERROR] guild_settings not found onCount.py (Guild: {ctx.guild.id}, ID: {ctx.guild.id})")
+            db = mysql.connector.connect(host=settings.host, database=settings.database,
+                                         user=settings.user, passwd=settings.passwd)
+            cursor = db.cursor()
+
+            # Remove all data from database.
+            cursor.execute("DELETE FROM guildSettings WHERE guild = %s", (ctx.guild.id,))
+            cursor.execute("DELETE FROM guildData WHERE guild = %s", (ctx.guild.id,))
+            cursor.execute("DELETE FROM guildModules WHERE guild = %s", (ctx.guild.id,))
+            db.commit()
+
+            # Insert all data into database.
+            cursor.execute(settings.insert_guildsettings, (ctx.guild.id, f"{prefix}", channel_id, maxcount, timeoutrole, 0, language))
+            cursor.execute(settings.insert_guilddata, (ctx.guild.id, count, lastuser))
+            cursor.execute(settings.insert_guildmodules, (ctx.guild.id, modules[0], modules[1], modules[2], modules[3], modules[4]))
+            db.commit()
+
+            for user in userdata:
+                try:
+                    user_count = int(userdata[user])
+                    user_id = int(user)
+
+                    if user_count >= 0 and user_id > 0:
+                        cursor.execute("DELETE FROM userData WHERE guild = %s AND user = %s", (ctx.guild.id, user_id))
+                        db.commit()
+
+                        cursor.execute(settings.insert_userdata, (ctx.guild.id, user_id, user_count))
+                        db.commit()
+                except Exception:
+                    pass
+
+            users = []
+            for notification in notifications:
+                try:
+                    user_id = int(notifications[notification]['user'])
+                    mode = int(notifications[notification]['mode'])
+                    number = int(notifications[notification]['number'])
+
+                    if mode == 1 or mode == 0:
+                        pass
+                    else:
+                        continue
+
+                    if number <= 0:
+                        continue
+
+                    if user_id <= 0:
+                        continue
+
+                    if user_id not in users:
+                        users.append(user_id)
+                        cursor.execute("DELETE FROM userNotify WHERE guild = %s AND user = %s", (ctx.guild.id, user_id))
+                        db.commit()
+
+                    cursor.execute(settings.insert_usernotify, (ctx.guild.id, user_id, number, mode))
+                    db.commit()
+                except Exception:
+                    pass
+
+            for flow in automation:
+                try:
+                    trigger = int(automation[flow]['trigger'])
+                    action = int(automation[flow]['action'])
+                    number = int(automation[flow]['number'])
+                    action_id = int(automation[flow]['actionId'])
+                    msg = automation[flow]['message']
+
+                    if number >= 10:
+                        cursor.execute("DELETE FROM guildAutomation WHERE guild = %s AND triggerNr = %s AND action = %s AND number = %s", (ctx.guild.id, trigger, action, number))
+                        db.commit()
+
+                        cursor.execute(settings.insert_guildautomation, (ctx.guild.id, trigger, action, number, action_id, msg))
+                        db.commit()
+                except Exception:
+                    pass
+
+            await ctx.send(":white_check_mark: Imported all data.\n"
+                           "*Some settings/data might not be imported correctly if you changed the file manually.*")
             db.close()
-            return
-
-        cursor.execute("SELECT * FROM guildData WHERE guild = %s", (ctx.guild.id,))
-        guild_data = cursor.fetchone()
-
-        if guild_data is None:
-            guild_data = (ctx.guild.id, 0, 0)
-
-        cursor.execute("SELECT * FROM guildModules WHERE guild = %s", (ctx.guild.id,))
-        guild_modules = cursor.fetchone()
-
-        if guild_modules is None:
-            guild_modules = (ctx.guild.id, 0, 0, 0, 0, 0)
-
-        cursor.execute("SELECT * FROM guildAutomation WHERE guild = %s", (ctx.guild.id,))
-        guild_automation = cursor.fetchall()
-
-        if guild_automation is None:
-            guild_automation = ()
-
-        cursor.execute("SELECT * FROM userData WHERE guild = %s", (ctx.guild.id,))
-        user_data = cursor.fetchall()
-
-        if user_data is None:
-            user_data = ()
-
-        cursor.execute("SELECT * FROM userNotify WHERE guild = %s", (ctx.guild.id,))
-        user_notify = cursor.fetchall()
-
-        if user_notify is None:
-            user_notify = ()
-
-        db.close()
-
-        user_data_dict = {}
-        for user in user_data:
-            user_data_dict[f"{user[2]}"] = user[3]
-
-        notifications_data = {}
-        for notification in user_notify:
-            user_data_dict[f"{notification[0]}"] = {
-                "user": notification[2],
-                "mode": notification[4],
-                "number": notification[3]
-            }
-
-        automation_data = {}
-        for automation in guild_automation:
-            user_data_dict[f"{automation[0]}"] = {
-                "trigger": automation[2],
-                "action": automation[3],
-                "extra": automation[4]
-            }
-
-        try:
-            os.remove(f"data/exportdata/{ctx.guild.id}.json")
-        except Exception:
-            pass
-
-        export_data = {
-            "guild-id": ctx.guild.id,
-            "prefix": guild_settings[1],
-            "channel-id": guild_settings[2],
-            "maxcount": guild_settings[3],
-            "timeoutrole": guild_settings[4],
-            "language": guild_settings[6],
-            "count": guild_data[1],
-            "lastuser": guild_data[2],
-            "modules": {
-                "allow-spam": guild_modules[1],
-                "restart-error": guild_modules[2],
-                "emote-react": guild_modules[3],
-                "recover-mode": guild_modules[4],
-                "post-embed": guild_modules[5]
-            },
-            "userdata": user_data_dict,
-            "notifications": notifications_data,
-            "automation": automation_data
-        }
-
-        with open(f"data/exportdata/{ctx.guild.id}.json", "w") as export_file:
-            json.dump(export_data, export_file, indent=4)
-
-        try:
-            await ctx.author.send(f"Database Export data for guild {ctx.guild} ({ctx.guild.id})", file=discord.File(f"data/exportdata/{ctx.guild.id}.json"))
-            await ctx.send(":white_check_mark: Export Data send into you DMs.")
-        except discord.errors.Forbidden:
-            await ctx.send(":x: I cannot send a message to you. Please enable private messaging in the privacy settings of this server.")
 
 
 def setup(client):
